2dd52605cb31f185f05dbebed0a8b9b2ce65cc86
John Keno Iweh <iwehjohn@gmail.com>
2025-08-19 16:08:17 +0100

    setup: implement routing and add demo login navigation in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { Link } from "react-router";
 import "./App.css";
 import { ThemeProvider } from "./components/theme-provider";
+import { Button } from "./components/ui/button";
+import { ArrowRight } from "lucide-react";
 
 function App() {
 	return (
@@ -7,6 +10,12 @@ function App() {
 			<div className="justify-center items-center flex flex-col h-screen">
 				<h1 className="text-2xl font-bold">Welcome to Ease Job</h1>
 				<p className="text-lg">Your job search made easy!</p>
+				<Button asChild>
+					<Link to={"login"}>
+						click to nav to login
+						<ArrowRight />
+					</Link>
+				</Button>
 			</div>
 		</ThemeProvider>
 	);
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+	{
+    path: "/login",
+    element: <div>Login Page</div>,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
