7fd294677a4af3cc5ea2a8ed5ba70b1126905dee
John Keno Iweh <iwehjohn@gmail.com>
2025-08-30 09:06:09 +0100

    feat: Update WhyEaseJob component with new icons and content

--- src/components/common/why-ease-job.tsx
+++ src/components/common/why-ease-job.tsx
@@ -3,6 +3,9 @@ import { Link } from "react-router";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { LuMoveRight } from "react-icons/lu";
 import Group3Icon from "@/assets/group_3_line.svg?react";
+import TimeDuration from "@/assets/time_duration.svg?react";
+import MarkedShield from "@/assets/marked_shield.svg?react";
+import NodeConnection from "@/assets/node_connection.svg?react";
 
 export function WhyEaseJob() {
 	return (
@@ -10,7 +13,7 @@ export function WhyEaseJob() {
 			<h1 className="text-4xl font-semibold text-center max-lg:text-2xl">
 				Connecting People. <br className="hidden max-lg:block" />
 				Creating Opportunities
-			</h1> 
+			</h1>
 			<div className="flex justify-between w-full mx-auto p-10 max-lg:flex-col-reverse max-lg:p-5 ">
 				{/**Left Item */}
 				<div className="w-2/5 space-y-20 p-20 mt-20 max-lg:space-y-0 max-lg:w-full max-lg:mt-0 max-lg:p-10">
@@ -47,13 +50,13 @@ export function WhyEaseJob() {
 					<Card className="gap-5 max-lg:order-1 ">
 						<CardHeader className="gap-5">
 							<CardTitle className="space-y-5">
-								<Group3Icon />
-								<h1 className="text-xl">Built for Everyone</h1>
+								<NodeConnection />
+								<h1 className="text-xl">Direct Connection</h1>
 							</CardTitle>
 							<CardContent className="w-full px-0">
 								<p className="text-wrap">
-									From skilled artisans and experienced professionals to
-									recruiters and agents, our tools are designed to work for all.
+									Skip the middleman — connect directly with decision-makers,
+									speed up conversations, and get faster results.
 								</p>
 							</CardContent>
 						</CardHeader>
@@ -61,13 +64,13 @@ export function WhyEaseJob() {
 					<Card className="gap-5 max-lg:order-3 ">
 						<CardHeader className="gap-5">
 							<CardTitle className="space-y-5">
-								<Group3Icon />
-								<h1 className="text-xl">Built for Everyone</h1>
+								<TimeDuration />
+								<h1 className="text-xl">Easy to Use</h1>
 							</CardTitle>
 							<CardContent className="w-full px-0">
 								<p className="text-wrap">
-									From skilled artisans and experienced professionals to
-									recruiters and agents, our tools are designed to work for all.
+									Post a job or apply in minutes with clear, simple steps that
+									make the whole process smooth from start to finish.
 								</p>
 							</CardContent>
 						</CardHeader>
@@ -75,13 +78,13 @@ export function WhyEaseJob() {
 					<Card className="gap-5 max-lg:order-4">
 						<CardHeader className="gap-5">
 							<CardTitle className="space-y-5">
-								<Group3Icon />
-								<h1 className="text-xl">Built for Everyone</h1>
+								<MarkedShield />
+								<h1 className="text-xl">Verified & Trusted</h1>
 							</CardTitle>
 							<CardContent className="w-full px-0">
 								<p className="text-wrap">
-									From skilled artisans and experienced professionals to
-									recruiters and agents, our tools are designed to work for all.
+									We’re a trusted space, with verified employers, agents, and
+									candidates, making every connection safe and reliable.
 								</p>
 							</CardContent>
 						</CardHeader>
